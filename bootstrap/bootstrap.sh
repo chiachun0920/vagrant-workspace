5af3221d4af39773c9b238fc77159f9e4e45611c
chris <chiachun0920@gmail.com>
2019-05-20 17:50:05 +0800

    add install procedure for docker-compose

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -33,6 +33,13 @@ _install_docker () {
   
   sudo groupadd docker
   sudo gpasswd -a $USER docker
+
+  sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+
+  base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  sudo curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo install /tmp/docker-machine /usr/local/bin/docker-machine
 }
 
 _install () {
